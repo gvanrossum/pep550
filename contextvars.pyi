05e9500a9f47c0ff71076ff9a204d962b2b24057
Guido van Rossum <guido@python.org>
2017-09-05 16:17:18 -0700

    Expand EC into pseudo code

--- contextvars.pyi
+++ contextvars.pyi
@@ -58,13 +58,13 @@ class WrappedLocalContext:
     @property
     def lc(self) -> BareLocalContext: ...  # return self._lc
     def run(self, fn: Callable[..., T], *args: Any, **kwds: Any) -> T:
-        orig_ec = getEC()
+        orig_ec = get_EC()
         ec = ExecutionContext(self.lc, orig_ec)
         try:
             return fn(*args, **kwds)
         finally:
             self._lc = ec.lc
-            setEC(orig_ec)
+            set_EC(orig_ec)
 
 class ExecutionContext:
     """Execution context -- a linked list of BareLocalContexts.
@@ -72,14 +72,37 @@ class ExecutionContext:
     To push a local context, use ExecutionContext(lc, ec).
     To pop a local context, use ec.back.
     """
-    def __init__(self, lc: BareLocalContext, back: Optional[ExecutionContext]) -> None: ...
-    @property
-    def depth(self) -> int: ...
-    @property
-    def lc(self) -> BareLocalContext: ...
-    @property
-    def back(self) -> ExecutionContext: ...
-    def run(self, fn: Callable[..., T], *args, **kwds) -> T: ...  # Pushes empty LC and calls callable
 
-def getEC() -> ExecutionContext: ...  # Return current thread's EC
-def setEC(ec: ExecutionContext) -> None: ...  # Set current thread's EC
+    def __init__(self, lc: BareLocalContext, back: Optional[ExecutionContext]) -> None:
+        self._lc = lc
+        self._back = back
+
+    @property
+    def depth(self) -> int:
+        """Number of links in the chain (>= 1)"""
+        if self.back is None:
+            return 1
+        return self.back.depth + 1
+
+    @property
+    def lc(self) -> BareLocalContext:
+        return self._lc
+
+    @property
+    def back(self) -> ExecutionContext:
+        return self._back
+
+    def squash(self) -> ExecutionContext: ...  # Return an equivalent EC with depth 1
+
+def get_EC() -> ExecutionContext: ...  # Return current thread's EC
+
+def set_EC(ec: ExecutionContext) -> None: ...  # Set current thread's EC
+
+def run_with_EC(fn: Callable[..., T], *args, **kwds) -> T:
+    """Pushes empty LC and calls callable"""
+    ec = get_EC()
+    try:
+        set_EC(ExecutionContext(BareLocalContext(), ec))
+        return fn(*args, **kwds)
+    finally:
+        set_EC(ec)
